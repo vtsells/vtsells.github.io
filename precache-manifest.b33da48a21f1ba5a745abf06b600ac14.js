8986a4e38e2dbfc7577ef22a3144de39bd4759ed
Tyler Sells <vtsells@yahoo.com>
2019-03-25 00:08:45 -0500

    Completely rewritten as a react app instead of static HTML

---
+++ precache-manifest.b33da48a21f1ba5a745abf06b600ac14.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a3cf53ed7ef9e4f2c3a5bac95d2e66",
+    "url": "/static/media/LambdaActions.40a3cf53.PNG"
+  },
+  {
+    "revision": "1e391f1524e3ec8e9649",
+    "url": "/static/css/main.ed639f18.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "847bfcde1f39e3b89e2e",
+    "url": "/static/js/2.31efc57b.chunk.js"
+  },
+  {
+    "revision": "3aa3ce54b0eab1c87b52b14a9a19cfec",
+    "url": "/static/media/me.3aa3ce54.jpg"
+  },
+  {
+    "revision": "191e9a54b656df4f8fd9091a1c057c11",
+    "url": "/static/media/WeatherApp.191e9a54.PNG"
+  },
+  {
+    "revision": "c4e9285cd45d96dc7ae65cd575406f31",
+    "url": "/static/media/Blog.c4e9285c.PNG"
+  },
+  {
+    "revision": "f36a01d0b9b040d0c7631fee12e61f2d",
+    "url": "/static/media/HeaderYoutube.f36a01d0.png"
+  },
+  {
+    "revision": "54ea8cc665f0c247ee193bec06030788",
+    "url": "/static/media/RecordEditing.54ea8cc6.PNG"
+  },
+  {
+    "revision": "1e391f1524e3ec8e9649",
+    "url": "/static/js/main.09124ce3.chunk.js"
+  },
+  {
+    "revision": "27043f50ec8067d687d8913b64360387",
+    "url": "/static/media/JSFade.27043f50.png"
+  },
+  {
+    "revision": "7091104fdd413ec568d0d4b24bbbb8d4",
+    "url": "/static/media/buildings.7091104f.jpg"
+  },
+  {
+    "revision": "3da9596022f4f77410fc6dcdbd3f88aa",
+    "url": "/static/media/roses.3da95960.jpg"
+  },
+  {
+    "revision": "c51dd2c7debf5ce7e2f8c19e19627d3b",
+    "url": "/static/media/toronto.c51dd2c7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6bd7ff60381fdf964b8cfe970b8ed08e",
+    "url": "/static/media/nav-bg.6bd7ff60.jpg"
+  },
+  {
+    "revision": "1db157452d728164c09bf418c40503b4",
+    "url": "/static/media/header-bg.1db15745.jpg"
+  },
+  {
+    "revision": "6929c9026ef076751fc7c5768964f781",
+    "url": "/index.html"
+  }
+];
